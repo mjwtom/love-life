50dd2a01e499b6895d69b7d250925f39773eb8f2
mjwtom <missing1988>
2016-09-24 02:20:56 +0800

    create test report database items.

--- src/stms/database/insert_docx.py
+++ src/stms/database/insert_docx.py
@@ -1,15 +1,14 @@
 from db_util import MysqlClient
 from statements import instance_insert, metadata_insert, structure_insert, \
-clean_instance, clean_structure, clean_metadata, instance_insert
-
+    clean_instance, clean_structure, clean_metadata, instance_insert
 
 conf = dict(
-    host = '115.28.239.239',
-    user = 'trhz',
-    password = 'zhimakaimen',
-    port = 3306,
-    database = 'trhz',
-    charset = 'utf8'
+    host='115.28.239.239',
+    user='trhz',
+    password='zhimakaimen',
+    port=3306,
+    database='trhz',
+    charset='utf8'
 )
 
 
@@ -26,28 +25,185 @@ def clean():
 
 def insert_test_report():
     init_map = dict(
-        className = 'com.stms.tps.doc.TestOutlineImpl',
-        methodName = 'initMetadata',
-        params =  ["projectId"]
+        className='com.stms.tps.doc.TestOutlineImpl',
+        methodName='initMetadata'
     )
     param_map = dict(
         projectId="13",
         testTurn="1"
     )
     order_num = 0
-    #instance_id = instance_insert(param_map, '13', init_map)
-    instance_id = instance_insert(param_map, '13')
-    metadata_params = ['projectId']
+    # instance_id = instance_insert(param_map, '13', init_map)
+    # 插入文档实例
+    instance_id = instance_insert(param_map, 13)
+    # 插入目录
     metadata_map = dict(
-        className = 'com.stms.tps.doc.TestReportImpl',
-        methodName = 'getTpsProductSpec',
-        params = metadata_params
+        className='com.stms.tps.doc.TestReportImpl',
+        methodName='getSimpleToC'
     )
     metadata_id = metadata_insert(metadata_map)
-    structure_insert(instance_id, order_num, metadata_id)
+    structure_insert(instance_id, -1, order_num, '3', 1,
+                     '目录', '生成目录', metadata_id);
     order_num += 1
-    structure_insert(instance_id, order_num)
+
+    # 插入分页符
+    metadata_map = dict(
+        className='com.stms.tps.doc.TestReportImpl',
+        methodName='getBreakPage'
+    )
+    break_page_metadata_id = metadata_insert(metadata_map)
+    structure_insert(instance_id, -1, order_num, '3', 1,
+                     '分页', '分页', break_page_metadata_id);
     order_num += 1
+
+    # 插入范围
+    pid1 = structure_insert(instance_id, -1, order_num, '1', 1,
+                     '范围', '范围d');
+    order_num += 1
+
+    # 插入文档标识
+    pid = structure_insert(instance_id, pid1, order_num, '1', 2,
+                     '标识', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '1) 文档标识号：XXXX-RJCP-CPDG-XXXX 。');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '2) 标题： YYYYYYYY软件定型/鉴定测评大纲 。');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '3) 被测件：YYYYYYYY软件 ，各软件配置项信息及标识见表1-1。');
+    order_num += 1
+
+    metadata_map = dict(
+        className='com.stms.tps.doc.TestReportImpl',
+        methodName='getVersionInformation',
+        params=['projectId']
+    )
+    # 插入测试项数据用于处理复杂的节点
+    metadata_id = metadata_insert(metadata_map)
+    structure_insert(instance_id, pid,order_num, '3', 1,
+                     '版本信息表', '测试项', metadata_id)
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '4)术语和缩略语');
+    order_num += 1
+
+    metadata_map = dict(
+        className='com.stms.tps.doc.TestReportImpl',
+        methodName='getTerms',
+        params=['projectId']
+    )
+    # 插入测试项数据用于处理复杂的节点
+    metadata_id = metadata_insert(metadata_map)
+    structure_insert(instance_id, pid, order_num, '3', 1,
+                     '术语和缩略语', '测试项', metadata_id)
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid1, order_num, '2', 2,
+                     '文档概述', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '本文档内容包括：1.范围；2.引用文档；3.测评概述；4.测试结果；5.评价结论与改进建议；6.其他。');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '文档标识本文档适用于XXXXXXXX系统YYYYYYYY软件定型/鉴定 测评，是XXXXXXXX系统YYYYYYYY软件定型/鉴定 的依据 。');
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid1, order_num, '1', 2,
+                     '委托方的名称与联系方式 ', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '； ', '委托方的名称：海定委办公室');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '委托方地址：XXXXXXXXXX； ');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     ' ', '委托方联系人：XXX；');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '委托方联系人电话：010-XXXXXX。 ');
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid1,order_num, '1', 2,
+                     '承研单位的名称与联系方式 ', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     ' ', '承研单位的名称：海定委办公室；');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '承研单位地址：XXXXXXXXXX； ');
+    order_num += 1
+
+    structure_insert(instance_id, pid ,order_num, '2', 2,
+                     ' ', '承研单位联系人：XXX；');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     ' ', '承研单位联系人电话：010-XXXXXX。');
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid1, order_num, '1', 2,
+                     '定型/鉴定 测评机构的名称与联系方式 ', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '定型/鉴定 测评机构的名称：海定委办公室； ');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     ' ', '定型/鉴定 测评机构地址：XXXXXXXXXX；');
+    order_num += 1
+
+    structure_insert(instance_id, pid,order_num, '2', 2,
+                     ' ', '定型/鉴定 测评机构联系人：XXX；');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '2', 2,
+                     '', '定型/鉴定 测评机构联系人电话：010-XXXXXX。 ');
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid1,order_num, '1', 2,
+                     '被测软件概述', '文档标识');
+    order_num += 1
+
+    pid = structure_insert(instance_id, pid, order_num, '1', 3,
+                     '系统概述', '文档标识');
+    order_num += 1
+
+    structure_insert(instance_id, pid, order_num, '1', 3,
+                     '系统的使命任务和组成 ', '文档标识');
+    order_num += 1
+
+    metadata_map = dict(
+        className='com.stms.tps.doc.TestReportImpl',
+        methodName='getTpsProductSpec',
+        params=['projectId']
+    )
+    # 插入测试项数据用于处理复杂的节点
+    metadata_id = metadata_insert(metadata_map)
+    structure_insert(instance_id, pid, order_num, '3', 1,
+                     '测试项', '测试项', metadata_id)
+    order_num += 1
+
+    pid1 = structure_insert(instance_id, -1, order_num, '1', 1,
+                     '引用文件', '文档标识');
+    order_num += 1
+
+
     print("doc instance id %s" % instance_id)
 
 
--- src/stms/database/statements.py
+++ src/stms/database/statements.py
@@ -2,54 +2,55 @@ import json
 from db_util import MysqlClient
 
 conf = dict(
-    host = '115.28.239.239',
-    user = 'trhz',
-    password = 'zhimakaimen',
-    port = 3306,
-    database = 'trhz',
-    charset = 'utf8'
+    host='115.28.239.239',
+    user='trhz',
+    password='zhimakaimen',
+    port=3306,
+    database='trhz',
+    charset='utf8'
 )
 
 
 def metadata_insert(map):
     url = json.dumps(map)
-    data = ('test', #metadata的名称
-            'test', #摘要
-            'majingwei', #签名，暂时应该没有用处
-            url, #指定url，说明用哪个类的哪个函数来处理
-            'majingwei',
+    data = ('test',  # metadata的名称
+            'test',  # 摘要
+            '4',  # 签名，暂时应该没有用处
+            url,  # 指定url，说明用哪个类的哪个函数来处理
+            1013,
             '0')
     sql = 'INSERT INTO trhz.docmetadata (metadata, digest, sign, url, addedBy, deleted) ' \
           'VALUES (%s, %s, %s, %s, %s, %s)'
     client = MysqlClient(conf)
     client.insert(sql, data)
-    sql = 'select id from trhz.docmetadata where metadata = "test" and addedBy = "majingwei" and url = \'%s\'' % url
+    sql = 'select id from trhz.docmetadata where metadata = "test" and addedBy = 1013 and url = \'%s\'' % url
     r = client.select(sql)
     if r:
         return r[0][0]
     else:
         return None
 
-
-def structure_insert(instanceId, order_num, metadataId=None):
-    data =('-1',
-           instanceId,
-           order_num,
-           '2',
-           '3',
-           metadataId,
-           'test',
-           'test',
-           'majingwei',
-           '0')
+#code '1'章节 '2'正文
+def structure_insert(instanceId, pid,order_num, code, level,
+                     name, digest, metadataId=None):
+    data = (pid,
+            instanceId,
+            order_num,
+            code,
+            level,
+            metadataId,
+            name,
+            digest,
+            1013,
+            '0')
     sql = 'INSERT INTO trhz.docinstancestru ' \
           '(pId, insId, orderNum, struSign, struLevel, metadataId, struName, struDigest, addedBy, deleted) ' \
           'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     client = MysqlClient(conf)
     client.insert(sql, data)
     sql = 'select id from trhz.docinstancestru ' \
-          'where insId = "%s" and orderNum = "%s" and addedBy = "majingwei" and metadataId = "%s"' \
-          % (instanceId, order_num, metadataId)
+          'where insId = %d and orderNum = %d and addedBy = 1013' \
+          % (instanceId, order_num)
     r = client.select(sql)
     if r:
         return r[0][0]
@@ -57,13 +58,14 @@ def structure_insert(instanceId, order_num, metadataId=None):
         return None
 
 
-def instance_insert(param_map, projectId, init_map=None):
+def instance_insert(param_map, projectId, id=None, init_map=None):
     params = json.dumps(param_map)
     if init_map:
         init = json.dumps(init_map)
     else:
         init = None
-    data = ('1',
+    data = (id,
+            '1',
             '2',
             'test',
             'test',
@@ -71,23 +73,21 @@ def instance_insert(param_map, projectId, init_map=None):
             'test',
             params,
             projectId,
-            'majingwei',
-            '2016-08-28 18:22:55',
-            'majingwei',
-            '2016-08-28 21:12:01',
+            1013,
+            1013,
             '0',
             init,
             'test.docx'
-    )
+            )
     sql = 'INSERT INTO trhz.docinstance' \
-          '(libId, tplId, insName, insDigest, identifier, version, params, projectId,' \
-          'addedBy, addedTime, editedBy, editedTime, deleted, metadata, physicalFileName) ' \
+          '(id, libId, tplId, insName, insDigest, identifier, version, params, projectId,' \
+          'addedBy, editedBy, deleted, metadata, physicalFileName) ' \
           'VALUES ' \
-          '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+          '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     client = MysqlClient(conf)
     client.insert(sql, data)
-    sql = 'select id from trhz.docinstance where params = \'%s\' and addedBy = "majingwei"' \
-          ' and projectId = "%s"' \
+    sql = 'select id from trhz.docinstance where params = \'%s\' and addedBy = 1013' \
+          ' and projectId = %d' \
           % (params, projectId)
     r = client.select(sql)
     if r:
@@ -97,15 +97,15 @@ def instance_insert(param_map, projectId, init_map=None):
 
 
 def clean_instance():
-    sql = 'delete from trhz.docinstance where addedBy = "majingwei"'
+    sql = 'delete from trhz.docinstance where addedBy = 1013'
     return sql
 
 
 def clean_structure():
-    sql = 'delete from trhz.docinstancestru where addedBy = "majingwei"'
+    sql = 'delete from trhz.docinstancestru where addedBy = 1013'
     return sql
 
 
 def clean_metadata():
-    sql = 'delete from trhz.docmetadata where addedBy = "majingwei"'
+    sql = 'delete from trhz.docmetadata where addedBy = 1013'
     return sql
