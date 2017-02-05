46d327718f23bbac58aba176a490d4d8fc02a66c
majingwei <majingwei@baidu.com>
2017-02-05 15:21:22 +0800

    plan bcloud 2 cmake

---
+++ src/cds/bcloud2cmake.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''
+author: majingwei
+email: majingwei@baidu.com
+I like to use python3 to construct the CMakeList.txt
+'''
+
+import os
+
+
+
+def 
+
+def start_generate(path=None):
+    if not path:
+        path = os.path.dirname(os.path.abspath(__file__))
+    bcloud_file = path+os.path.join(path, 'BCLOUD')
+
+
+if __name__ == '__main__':
+    start_generate()
